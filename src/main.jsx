58ee2bc822d16f7765ca6510093bea601a73e99b
Fahad <127460043+fahadfakrul@users.noreply.github.com>
2024-03-20 12:17:21 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './Components/Home/Home';
+import About from './Components/About/About';
+import Contact from './Components/Contact/Contact';
+import Users from './Components/Users/Users';
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+   children: [ 
+    {
+      path: "/about",
+      element: <About></About>,
+    },
+    {
+      path: "/contact",
+      element: <Contact></Contact>,
+    },
+    {
+      path: "/users",
+      element: <Users></Users>,
+    },
+   ]
+  },
+  
+ 
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+     <RouterProvider router={router} />
+  </React.StrictMode>,
+)
