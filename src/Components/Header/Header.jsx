225cccaffb09c1238f4f38433bccb7983de8199e
Fahad <127460043+fahadfakrul@users.noreply.github.com>
2024-03-20 14:42:09 +0600

    final commit

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,12 +1,14 @@
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import './Header.css'
 const Header = () => {
     return (
         <div>
             <span>My website</span>
             <nav>
+            
                 <Link to="/">Home</Link>
-                <Link to="/users">Users</Link>
+                <NavLink to="/users">Users</NavLink>
+                <NavLink to="/posts">Posts</NavLink>
                 <Link to="/about">About</Link>
                 <Link to="/Contact">Contact Us</Link>
                 
--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,11 @@ import Home from './Components/Home/Home';
 import About from './Components/About/About';
 import Contact from './Components/Contact/Contact';
 import Users from './Components/Users/Users';
+import UserDetails from './Components/UserDeatils/UserDetails.jsx';
+import Posts from './Components/Posts/Posts.jsx';
+import PostDetails from './Components/PostDetails/PostDetails.jsx';
+import ErrorPage from './Components/ErrorPage/ErrorPage.jsx';
+
 
 
 
@@ -17,6 +22,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <Home></Home>,
+    errorElement:<ErrorPage></ErrorPage>,
    children: [ 
     {
       path: "/about",
@@ -28,8 +34,24 @@ const router = createBrowserRouter([
     },
     {
       path: "/users",
+      loader: () => fetch('https://jsonplaceholder.typicode.com/users'),
       element: <Users></Users>,
     },
+    {
+      path: "/user/:userId",
+      loader: ({params}) => fetch(`https://jsonplaceholder.typicode.com/users/${params.userId}`),
+      element: <UserDetails></UserDetails>
+    },
+    {
+      path: '/posts', 
+      loader: () => fetch('https://jsonplaceholder.typicode.com/posts'),
+      element: <Posts></Posts>
+    }, 
+    {
+      path: '/post/:postId', 
+      loader: ({params}) => fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`),
+      element: <PostDetails></PostDetails>
+    }
    ]
   },
   
